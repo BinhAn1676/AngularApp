4055ad8c53d36d8346ff26e930e3e4fd4e0cf4c0
Admin <andepzai2192003@gmail.com>
2024-05-01 01:19:14 +0700

    working on form checkout

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -45,16 +45,31 @@ export class CartService {
     //publish event 
     this.totalPrice.next(totalPriceValue);
     this.totalQuantity.next(totalQuantityValue);
-    this.logCartData(totalPriceValue,totalQuantityValue);
+    this.logCartData(totalPriceValue, totalQuantityValue);
   }
   logCartData(totalPriceValue: number, totalQuantityValue: number) {
     console.log('content of cart');
-    for(let tempCartItem of this.cartItems){
+    for (let tempCartItem of this.cartItems) {
       const subTotalPrice = tempCartItem.quantity * tempCartItem.unitPrice;
       console.log(`name: ${tempCartItem.name},quantity: ${tempCartItem.quantity},unitPrice: ${tempCartItem.unitPrice},subTotalPrice : ${subTotalPrice}`);
 
     }
     console.log(`totalPrice: ${totalPriceValue.toFixed(2)},totalQuantity:${totalQuantityValue}`);
+  }
+  decrementQuantity(theCartItem: CartItem) {
+    theCartItem.quantity--;
+    if (theCartItem.quantity === 0) {
+      this.remove(theCartItem);
+    } else {
+      this.computeCartTotals();
+    }
 
+  }
+  remove(theCartItem: CartItem) {
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id === theCartItem.id);
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
   }
 }
