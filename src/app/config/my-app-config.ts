8f0eaf865e8378667ff9f4898ad2a5d89647b9c5
Admin <andepzai2192003@gmail.com>
2024-05-26 15:12:11 +0700

    done simple login

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc : {
+        clientId :'0oah6u8251UW828xY5d7',
+        issuer: 'https://dev-73870722.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid','profile','email']
+    }
+}
